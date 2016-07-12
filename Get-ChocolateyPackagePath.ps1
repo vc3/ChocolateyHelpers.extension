0fe09a09f52a081f68259db6f1b1d2b42e54a5f3
Bryan Matthews <mattheyan@gmail.com>
2016-07-12 16:59:02 -0400

    Initial commit

---
+++ Get-ChocolateyPackagePath.ps1
@@ -0,0 +1,20 @@
+[CmdletBinding()]
+param(
+    [string]$CommandPath=$MyInvocation.PSCommandPath
+)
+
+$commandName = Split-Path $CommandPath -Leaf
+
+if ($commandName -ne 'chocolateyInstall.ps1' -and $commandName -ne 'chocolateyUninstall.ps1') {
+    Write-Error "Cannot get package path from unsupported script '$($CommandPath)'."
+    return
+}
+
+$commandDir = Split-Path $CommandPath -Parent
+$commandDirName = Split-Path $commandDir -Leaf
+
+if ($commandDirName -eq 'tools') {
+    Write-Output (Split-Path $commandDir -Parent)
+} else {
+    Write-Output $commandDir
+}
